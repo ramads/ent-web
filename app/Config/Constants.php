c98b8a25f8e815ad942e39ffba5d0e56714d223c
Ramaditia D <rdwiyan.saputra@gmail.com>
2024-08-12 17:11:52 +0800

    add dynamic baseurl

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,3 +92,7 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
+
+$base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . (substr(dirname($_SERVER['SCRIPT_NAME']), -1) == '/' ? '' : '/');
+
+defined('BASE_URL') || define('BASE_URL', $base_url);
